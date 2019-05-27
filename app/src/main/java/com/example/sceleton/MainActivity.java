bca1abc0dcef6737935d6dd2fceb12a5ea82fa9b
wildeKaiserin <friedri5@hm.edu>
2019-05-27 13:41:54 +0200

    finished sceleton (first draft)

--- app/src/main/java/com/example/sceleton/MainActivity.java
+++ app/src/main/java/com/example/sceleton/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.sceleton;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.concurrent.Future;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,14 +24,53 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        //NATIVE CALL
+        setResolution(420.0f);
+
+        //NATIVE CALL
+        //file is dummy data, is ignored in the backend
+        openFile(new File("/page"));
+
+        //NATIVE CALL
+        mvFirstPage();
+
+        //create image object
+        Object pageImage = new Object();
+        ImageView imageView = findViewById(R.id.sample_image);
+        //NATIVE CALL
+        pageImage = getPage(imageView.getHeight(), imageView.getWidth());
+
+        //write image object into file
+        try {
+            FileWriter fw = new FileWriter("/page");
+            fw.write((String)pageImage);
+            fw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //decoding the saved image to display in the image view
+        Bitmap bitmap = BitmapFactory.decodeFile("/page");
+        imageView.setImageBitmap(bitmap);
+
+        //NATIVE CALL
+        closeFile();
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    //public native String stringFromJNI();
+
+    public native Object getPage(int height, int width);
+
+    public native boolean setResolution(float dpi);
+
+    public native boolean openFile(File hintfile);
+
+    public native boolean mvFirstPage();
+
+    public native boolean closeFile();
 }
